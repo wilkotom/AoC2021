c5f3f0cf08a550d3dc274c6fd282b1fb6ba81c83
Tom Wilkinson <tom+github@dentrasssi.net>
2021-12-01 06:21:13 +0000

    SeaQuest: AoC?

---
+++ day01/src/main.rs
@@ -0,0 +1,34 @@
+
+fn main() {
+    let data = std::fs::read_to_string("./input.txt").unwrap();
+    let numbers = data.split("\n").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
+    println!("Part 1: {}", part1(&numbers));
+    println!("Part 2: {}", part2(&numbers));
+}
+
+fn part1(numbers: &Vec<i32>) -> i32 {
+    let mut last_value = numbers[0];
+    let mut increment_count = 0;
+    for num in &numbers[1..] {
+        if *num > last_value {
+            increment_count +=1
+        }
+        last_value = *num;
+    
+    }
+    increment_count
+
+}
+
+fn part2(numbers: &Vec<i32>) -> i32 {
+    let mut sliding: Vec<i32> = Vec::new();
+    let mut current_value = numbers[0]+ numbers[1] + numbers[2];
+    sliding.push(current_value);
+    for i in 1..numbers.len()-2 {
+        current_value -= numbers[i-1];
+        current_value += numbers[i+2];
+        sliding.push(current_value);
+    }
+    part1(&sliding)
+
+}
