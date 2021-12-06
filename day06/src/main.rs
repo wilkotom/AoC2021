b451572bc3f5d61f53aa52e4304cbb6d7e010c63
Tom Wilkinson <tom.github@dentrasssi.net>
2021-12-06 08:41:00 +0000

    ETOOMANYFISH

---
+++ day06/src/main.rs
@@ -0,0 +1,13 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let mut lanternfish = VecDeque::from(vec![0 as usize; 9]);
+    for fish in std::fs::read_to_string("./input.txt").unwrap().split(",").map(|x| x.parse::<usize>().unwrap()){
+        lanternfish[fish] += 1;
+    }
+    for _ in 0..256 {
+        lanternfish.rotate_left(1);
+        lanternfish[6] += lanternfish[8];
+    }
+    println!("{}", lanternfish.iter().sum::<usize>())
+}
