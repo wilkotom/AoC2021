46f53c3171aeb55286b43f515ba39eb0b7d9ee0d
Tom Wilkinson <tom.github@dentrasssi.net>
2021-12-07 07:24:03 +0000

    Well of course crab submarines can only go sideways...

---
+++ day07/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let input = std::fs::read_to_string("./input.txt").unwrap().split(",").map(|v| v.parse::<isize>().unwrap()).collect::<Vec<_>>();
+    let smallest = *input.iter().min().unwrap_or(&isize::MIN);
+    let largest = *input.iter().max().unwrap_or(&isize::MAX);
+    let mut min_fuel_part1 = isize::MAX;
+    let mut min_fuel_part2 = isize::MAX;
+
+    for dest in smallest..largest+1 {
+        let mut part1_fuel = 0;
+        let mut part2_fuel = 0;
+        for sub in &input {
+            let dist = isize::abs(sub - dest);
+            part1_fuel += dist; //* number;
+            part2_fuel += dist * (dist +1) / 2;
+        }
+        if part1_fuel < min_fuel_part1 {
+            min_fuel_part1 = part1_fuel
+        }
+        if part2_fuel < min_fuel_part2 {
+            min_fuel_part2 = part2_fuel
+        }
+    }
+    println!("Minimum fuel Part 1: {}", min_fuel_part1);
+    println!("Minimum fuel Part 2: {}", min_fuel_part2);
+}
