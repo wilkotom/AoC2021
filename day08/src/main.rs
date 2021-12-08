a5d2076424a868108e3eebdbaf5b5a805bb09d1c
Tom Wilkinson <tom.github@dentrasssi.net>
2021-12-08 20:09:31 +0000

    Now with actual set logic, rather than shonky checks of len() on union

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -9,101 +9,64 @@ fn main() {
         let mut split = line.split(" | ");
         let input_section = split.next().unwrap();
         let output_section = split.next().unwrap();
-        for out in output_section.split(" ") {
-            if out.len() == 2 || out.len() == 3 || out.len() == 4 || out.len() == 7  {
-                counter +=1;
-            } if out.len() == 2 {
-
-            }
-        }
         let mut unconsidered = input_section.split(" ").collect::<VecDeque<_>>();
         while number_mappings.len() < 10 {
+            let nl = number_mappings.len();
+            let ul = unconsidered.len();
             let word = unconsidered.pop_front().unwrap();
-                match word.len() {
-                    2 => {
-                        number_mappings.insert(1, word.chars().collect::<HashSet<_>>());
+            let word_set = word.chars().collect::<HashSet<_>>();
+            match word_set.len() {
+                2 => {number_mappings.insert(1, word_set);},
+                3 => {number_mappings.insert(7, word_set);},
+                4 => {number_mappings.insert(4, word_set);},
+                7 => {number_mappings.insert(8, word_set);},
+                5 => {
+                    if number_mappings.contains_key(&1) && word_set.is_superset(number_mappings.get(&1).unwrap()) { 
+                        number_mappings.insert(3, word_set);
+                    } else if number_mappings.contains_key(&9) && word_set.is_subset(number_mappings.get(&9).unwrap()) {
+                        number_mappings.insert(5, word_set);
+                    } else if number_mappings.contains_key(&9) {
+                        number_mappings.insert(2, word_set);
+                    } else {
+                        unconsidered.push_back(word)
                     }
-                    3 =>  {
-                        number_mappings.insert(7, word.chars().collect::<HashSet<_>>());
-                    },
-                    4 => {
-                       number_mappings.insert(4, word.chars().collect::<HashSet<_>>());
-                    },
-
-                    5 => {
-                        // Could be 2, 3 or 5
-                        if number_mappings.contains_key(&9) {
-                            let word_set = word.chars().collect::<HashSet<_>>();
-                            if word_set.union(number_mappings.get(&9).unwrap()).count() == 7 {
-                                // if it doesn't completely overlap 9, it's a 2
-                                number_mappings.insert(2, word.chars().collect::<HashSet<_>>());
-                            } else {
-                                if number_mappings.contains_key(&1) {
-                                    if word_set.union(number_mappings.get(&1).unwrap()).count() == 6 {
-                                        // if doesn't completely overlap with 1, it's a 5
-                                        number_mappings.insert(5, word.chars().collect::<HashSet<_>>());
-                                    } else {
-                                        number_mappings.insert(3, word.chars().collect::<HashSet<_>>());
-
-                                    }
-                               } else {
-                                    unconsidered.push_back(word)
-                                }
-                            }
-                        } else {
-                            unconsidered.push_back(word)
-                        }
-                    },
-                    6 => {
-                        // Could be 0, 6 or 9
-                        if number_mappings.contains_key(&1) {
-                            let word_set = word.chars().collect::<HashSet<_>>();
-                            if word_set.union(number_mappings.get(&1).unwrap()).count() == 7 {
-                                // if it doesn't completely overlap with 1, it's a 6
-                                number_mappings.insert(6, word.chars().collect::<HashSet<_>>());
-                            } else {
-                                if number_mappings.contains_key(&4) {
-                                    if word_set.union(number_mappings.get(&4).unwrap()).count() == 7 {
-                                        // if it doesn't completely overlap with 4, it's a 0
-                                        number_mappings.insert(0, word.chars().collect::<HashSet<_>>());
-                                    } else {
-                                        // it's a 9
-                                        number_mappings.insert(9, word.chars().collect::<HashSet<_>>());
-                                    }
-                                } else {
-                                    unconsidered.push_back(word)
-                                }
-                                    
-                            }
-                        } else {
-                            unconsidered.push_back(word)
-                        }
+                },
+                6 => {
+                    if number_mappings.contains_key(&1) && !word_set.is_superset(number_mappings.get(&1).unwrap()) {
+                        number_mappings.insert(6, word.chars().collect::<HashSet<_>>());
+                    // Only consider the difference between 9 and 0 once 6 is identified.
+                    // 6 is not a superset of 4 or 1, so can't differentiate below until we've
+                    // identified it already.
+                    } else if number_mappings.contains_key(&6) && number_mappings.contains_key(&4) && word_set.is_superset(number_mappings.get(&4).unwrap()) {
+                        number_mappings.insert(9, word.chars().collect::<HashSet<_>>());
+                    } else if number_mappings.contains_key(&6) && number_mappings.contains_key(&4) {
+                        number_mappings.insert(0, word.chars().collect::<HashSet<_>>());
+                    } else {
+                        unconsidered.push_back(word)
                     }
-
-                    7 => {
-                        // it's a 8
-                        number_mappings.insert(8, word.chars().collect::<HashSet<_>>());
-                    }
-
-                    _ => {}
-                
-            
+                }, 
+                _ => {}
+            }
+            if number_mappings.len() == nl && unconsidered.len() != ul {
+                println!(" DANGER WILL ROBINSON");
             }
         }
-            let mut output = 0;
-            for word in output_section.split(" ") {
-                let char_set = word.chars().collect::<HashSet<_>>();
-                for num in number_mappings.keys() {
-                    if char_set == *number_mappings.get(num).unwrap() {
-                        output = (output*10) + num;
-                        break;
-                    }
+        let mut output = 0;
+        for word in output_section.split(" ") {
+            if word.len() >= 2 && word.len() <= 4 || word.len() == 7  {
+                counter +=1;
+            } 
+            let char_set = word.chars().collect::<HashSet<_>>();
+            for num in number_mappings.keys() {
+                if char_set == *number_mappings.get(num).unwrap() {
+                    output = (output*10) + num;
+                    break;
                 }
             }
-            total += output;
+        }
+        total += output;
     }
     println!("Part 1: {}", counter);
     println!("Part 2: {}", total);
-
 }
 
